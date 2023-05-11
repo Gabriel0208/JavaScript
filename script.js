187941f6e2f20c7f7f0a18c2fb4310fccd798c80
Gabriel0208 <126155865+Gabriel0208@users.noreply.github.com>
2023-05-11 15:21:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,19 +7,26 @@ function inserirdados(a) {
     b2 = parseFloat(prompt('Digite sua nota b2: '))
     m = (b1 + b2) / 2
     pr = parseFloat(prompt('Digite sua nota: '))
-    mf = (pr + m) / 2
+    mf = m
     tf = (faltas / a) * 100
 
     if (tf > 25) {
         s = 'Reprovado'
-        alert('Você foi reprovado')
+        alert('Você foi reprovado por falta')
     } else {
         if (m >= 7) {
             s = 'Aprovado'
             alert('Você foi aprovado')
-        } else if (mf >= 5) {
-            s = 'Aprovado'
-            alert('Você foi aprovado')
+        } else if (m < 7) {
+            mf = (pr + m) / 2
+            if (mf >= 5) {
+                s = 'Aprovado'
+                alert('Você foi aprovado')
+            }
+            else {
+                s = 'Reprovado'
+                alert('Você foi reprovado')
+            }
         } else {
             s = 'Reprovado'
             alert('Você foi reprovado')
@@ -30,10 +37,10 @@ function inserirdados(a) {
     linha.insertCell(0).innerText = matricula;
     linha.insertCell(1).innerText = nome;
     linha.insertCell(2).innerText = faltas;
-    linha.insertCell(3).innerText = b1;
-    linha.insertCell(4).innerText = b2;
-    linha.insertCell(5).innerText = m.toFixed(2);
-    linha.insertCell(6).innerText = pr.toFixed(2);
-    linha.insertCell(7).innerText = mf.toFixed(2);
+    linha.insertCell(3).innerText = b1.toFixed(1);;
+    linha.insertCell(4).innerText = b2.toFixed(1);;
+    linha.insertCell(5).innerText = m.toFixed(1);
+    linha.insertCell(6).innerText = pr.toFixed(1);
+    linha.insertCell(7).innerText = mf.toFixed(1);
     linha.insertCell(8).innerText = s;
 }
