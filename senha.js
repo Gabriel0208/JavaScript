98d1235b869ca43cd1b8f9882a59d4ea5d523c4d
Gabriel0208 <126155865+Gabriel0208@users.noreply.github.com>
2023-06-15 09:53:45 -0300

    Update senha.js

--- senha.js
+++ senha.js
@@ -1,25 +1,26 @@
-function validarSenha(senha) {
-  // Verifica o comprimento da senha
-  if (senha.length < 8) {
+function checksenha() {
+  s = prompt('Digie uma senha: ')
+
+  if (s.length < 8) {
     return false;
   }
 
-  // Verifica se contém pelo menos uma letra maiúscula, uma letra minúscula e um número
-  let temMaiuscula = false;
-  let temMinuscula = false;
-  let temNumero = false;
+  
+  let Mai = false;
+  let Min = false;
+  let Num = false;
 
-  for (let i = 0; i < senha.length; i++) {
-    if (senha[i] >= 'A' && senha[i] <= 'Z') {
-      temMaiuscula = true;
-    } else if (senha[i] >= 'a' && senha[i] <= 'z') {
-      temMinuscula = true;
-    } else if (senha[i] >= '0' && senha[i] <= '9') {
-      temNumero = true;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] >= 'A' && s[i] <= 'Z') {
+      Mai = true;
+    } else if (s[i] >= 'a' && s[i] <= 'z') {
+     Min = true;
+    } else if (s[i] >= '0' && s[i] <= '9') {
+      Num = true;
     }
 
     // Verifica se todos os critérios foram atendidos
-    if (temMaiuscula && temMinuscula && temNumero) {
+    if (Mai && Min && Num) {
       return true;
     }
   }
@@ -27,13 +28,3 @@ function validarSenha(senha) {
   return false;
 }
 
-// Exemplo de uso:
-let senha1 = 'SenhaSegura123';
-let senha2 = 'senhafraca';
-let senha3 = 'SENHAFORTE';
-let senha4 = '12345678';
-
-console.log(validarSenha(senha1)); // true
-console.log(validarSenha(senha2)); // false
-console.log(validarSenha(senha3)); // false
-console.log(validarSenha(senha4)); // false
